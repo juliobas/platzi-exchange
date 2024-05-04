35cdb57302b5122607c4152052ca1020d0f443b9
Julio Bastidas <juliobas@gmail.com>
2024-05-04 16:26:48 -0400

    Avance, fetch getAssets a la api de coincap

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,26 @@
 <template>
 <div>
-    <px-assets-table />
+    <px-assets-table :assets="assets"/>
 </div>
 </template>
 
 <script>
-import PxAssetsTable from '@/components/PxAssetsTable'
+import api from '@/api';
+import PxAssetsTable from '@/components/PxAssetsTable';
 
 
 export default {
     name: 'HomeView',
     components: {
         PxAssetsTable
+    },
+    data() {
+        return {
+            assets: []
+        }
+    },
+    created() {
+        api.getAssets().then(assets => (this.assets = assets));
     }
 }
 </script>
