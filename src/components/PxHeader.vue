d2632c6325b5d759600fa30b12c1e15309e6b565
Julio Bastidas <juliobas@gmail.com>
2024-05-10 17:36:16 -0400

    terminando el proyecto

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -7,7 +7,16 @@
                 <router-link :to="{ name: 'home'}" class="font-semibold text-xl tracking-tight">PlatziExchange</router-link>
             </div>
             <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-                <div class="text-sm lg:flex-grow"></div>
+                <div class="text-sm lg:flex-grow">
+                    <router-link
+                        v-for="l in links"
+                        :key="l.name"
+                        :to="l.to"
+                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
+                    >
+                        {{ l.title }}
+                    </router-link>
+                </div>
             </div>
         </nav>
         </nav>
@@ -21,6 +30,14 @@ export default {
     name: 'PxHeader',
     components: {
         PxIcon
+    },
+
+    props: {
+        links: {
+            type: Array,
+            default: () => []
+        }
     }
+
 }
 </script>
